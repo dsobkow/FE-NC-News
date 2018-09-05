a0f855860674d8d0a2e72fb9c05e170a8d462c96
dotty11 <dorota.sobkow@yahoo.co.uk>
2018-09-05 17:10:59 +0100

    article contents positioned

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -15,17 +15,16 @@ class Articles extends Component {
                     return (
                         <div className='article' key={index}>
                         <div className='votes'><i class="fas fa-arrow-circle-up"></i><p className='vote'>{article.votes}</p><i class="fas fa-arrow-circle-down"></i></div>
-                        <div>
                         <div className='user'>
                         <img className='avatar' src={article.created_by.avatar_url} onError={(e)=>{e.target.src='https://www.chaarat.com/wp-content/uploads/2017/08/placeholder-user.png'}} alt='avatar'/>
                          <div>posted by <strong>{article.created_by.username}</strong> {moment(article.created_at).fromNow()}</div>
                         </div>
+                        <div className='article_body'>
                         <p><NavLink className='article_title' to='/articles/:article_id'><strong><u>{article.title}</u></strong></NavLink></p>
-                        <p>{article.body}</p><br/>
-                        <div className='article_footer'><p className='comment_count'>{article.comments} comments</p>
+                        <p>{article.body}</p>
+                        </div>
+                        <p className='comment_count'>{article.comments} comments</p>
                         <p className='topic_info'>{article.belongs_to}</p></div>
-                        </div>
-                        </div>
                     )
                 })}
             </div>
